5056cac6632c31f84741afc07764e467a457b98e
Mohammed Aamir Khan <aam.khan1996@gmail.com>
2017-11-01 15:04:29 +0530

    first commit

---
+++ assets/js/rangeslider.js
@@ -0,0 +1,18 @@
+$("#range_08").ionRangeSlider({
+    grid: false,
+    hide_min_max: true,
+    from: 'Basic - 1',
+    force_edges: false,
+    prettify_enabled: true,
+    values: ["Basic - 1","Basic - 2","Basic - 3","Standard - 1","Standard - 2","Standard - 3","Advanced - 1","Advanced - 2","Advanced - 3"]
+});
+
+$("#range_09").ionRangeSlider({
+    grid: false,
+    hide_min_max: true,
+    from: 'Basic - 1',
+    force_edges: false,
+    prettify_enabled: true,
+    values: ["Basic - 1","Basic - 2","Basic - 3","Standard - 1","Standard - 2","Standard - 3","Advanced - 1","Advanced - 2","Advanced - 3"]
+    
+});
